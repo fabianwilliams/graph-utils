1d8eac8937296157ceaf4a155fda9c2f8e2fac3b
fabian williams <fabian.williams@microsoft.com>
2025-03-22 22:13:08 -0400

    fixed files with prettier write

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,28 +1,27 @@
-import { toGraphTime, fromGraphTime, buildGraphUrl } from "./index.mts";
-import { test, expect } from "vitest";
+import { toGraphTime, fromGraphTime, buildGraphUrl } from './index.mts';
+import { test, expect } from 'vitest';
 
-test("converts to ISO string", () => {
-  const date = new Date("2025-03-22T14:35:00.000Z");
-  expect(toGraphTime(date)).toBe("2025-03-22T14:35:00.000Z");
+test('converts to ISO string', () => {
+  const date = new Date('2025-03-22T14:35:00.000Z');
+  expect(toGraphTime(date)).toBe('2025-03-22T14:35:00.000Z');
 });
 
-test("parses ISO string", () => {
-  const iso = "2025-03-22T14:35:00.000Z";
+test('parses ISO string', () => {
+  const iso = '2025-03-22T14:35:00.000Z';
   const result = fromGraphTime(iso);
   expect(result.toISOString()).toBe(iso);
 });
 
-test("buildGraphUrl works", () => {
-  const urlStr = buildGraphUrl("/users", {
+test('buildGraphUrl works', () => {
+  const urlStr = buildGraphUrl('/users', {
     $filter: "startswith(displayName,'F')",
-    $select: "id,displayName"
+    $select: 'id,displayName',
   });
 
   const url = new URL(urlStr);
   const params = url.searchParams;
 
-  expect(url.pathname).toBe("/v1.0/users");
-  expect(params.get("$filter")).toBe("startswith(displayName,'F')");
-  expect(params.get("$select")).toBe("id,displayName");
+  expect(url.pathname).toBe('/v1.0/users');
+  expect(params.get('$filter')).toBe("startswith(displayName,'F')");
+  expect(params.get('$select')).toBe('id,displayName');
 });
-
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_mts_1 = require("./src/index.mts");
-console.log("ISO time:", (0, index_mts_1.toGraphTime)(new Date()));
-console.log("Parsed date:", (0, index_mts_1.fromGraphTime)("2025-03-22T14:35:00.000Z"));
-const url = (0, index_mts_1.buildGraphUrl)("/users", {
-    "$filter": "startswith(displayName,'F')",
-    "$select": "id,displayName",
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const index_mts_1 = require('./src/index.mts');
+console.log('ISO time:', (0, index_mts_1.toGraphTime)(new Date()));
+console.log(
+  'Parsed date:',
+  (0, index_mts_1.fromGraphTime)('2025-03-22T14:35:00.000Z'),
+);
+const url = (0, index_mts_1.buildGraphUrl)('/users', {
+  $filter: "startswith(displayName,'F')",
+  $select: 'id,displayName',
 });
-console.log("Built URL:", url);
+console.log('Built URL:', url);
 //# sourceMappingURL=test.js.map
