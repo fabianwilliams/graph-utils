50b0d8a5509ec2da69b59c0a183a03a5cc272a47
fabian williams <fabian.williams@microsoft.com>
2025-03-22 22:09:52 -0400

    added GithubWorkflow for CI test

--- test.ts
+++ test.ts
@@ -1,11 +1,10 @@
-import { toGraphTime, fromGraphTime, buildGraphUrl } from './src/index.js';
+import { toGraphTime, fromGraphTime, buildGraphUrl } from "./src/index.mts";
 
-console.log('ISO Time:', toGraphTime(new Date()));
-console.log('Parsed Date:', fromGraphTime('2025-03-22T14:35:00.000Z'));
+console.log("ISO time:", toGraphTime(new Date()));
+console.log("Parsed date:", fromGraphTime("2025-03-22T14:35:00.000Z"));
 
-const url = buildGraphUrl('/users', {
-  $filter: "startswith(displayName,'F')",
-  $select: 'id,displayName',
+const url = buildGraphUrl("/users", {
+  "$filter": "startswith(displayName,'F')",
+  "$select": "id,displayName",
 });
-
-console.log('Graph URL:', url);
+console.log("Built URL:", url);
